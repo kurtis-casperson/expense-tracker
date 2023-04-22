413f161ad74c52644f526a2b1d0b72f81d3ff59b
Kurtis <kcasperson7@gmail.com>
2023-04-22 12:31:36 -0700

    Change prop names

--- src/components/CreateExpense.js
+++ src/components/CreateExpense.js
@@ -1,69 +1,54 @@
-import React, { useState } from 'react'
-
-// let expenseDescription = document.getElementById('expenseDescription')
-// let expenseAmount = document.getElementById('expenseAmount')
-
-// const expenseObject = {
-//   // date:
-//   paymentType: ['Card', 'Cash', 'Crypto'],
-//   amount: '',
-//   merchant: '',
-// }
+import React from 'react'
 
 const CreateExpense = (props) => {
-  const { expense, setExpense } = props
+  const { formData, setFormData } = props
 
   // setexpense will refer back to the previous value when there is a change
-  //
-  // const [expenses, setExpense] = useState(expenseObject)
 
   function handleInputChange(event) {
     const { name, value } = event.target
 
-    setExpense((prevExpense) => ({ ...prevExpense, [name]: value }))
-
-    // if (expenses.amount === '') return
+    setFormData((prevExpense) => ({ ...prevExpense, [name]: value }))
   }
-
-  // if (this.amount === '')
-  // expenseDescription = expenses.description
-  // expenseAmount = expenses.amount
 
   return (
     <>
       <form className="expense-input-container">
-        <div id="date">
-          <input type="date" placeholder="Date"></input>
+        <div className="date">
+          <input
+            type="date"
+            placeholder="Date"
+            name="date"
+            value={formData.date}
+            onChange={handleInputChange}
+          ></input>
         </div>
         <div className="merchant-type" placeholder="Merchant Type">
           <input
             type="text"
             name="merchant"
             placeholder="Merchant Type"
-            value={expense?.merchant || ''}
+            value={formData.merchant}
             onChange={handleInputChange}
           ></input>
         </div>
 
-        <div id="paymentType">
-          <select required>
-            <option id="" disabled selected>
-              Payment Type
-            </option>
-            <option name="Card">Card</option>
-            <option name="Cash">Cash</option>
-            <option name="Crypto">Crypto</option>
-          </select>
+        <div className="payment-type">
+          <input
+            type="text"
+            name="paymentType"
+            placeholder="Cash, Card, or Crypto.."
+            value={formData.paymentType}
+            onChange={handleInputChange}
+          ></input>
         </div>
-        <div id="expenseAmount">
+        <div className="expense-amount">
           <input
             type="number"
             placeholder="Amount"
             name="amount"
-            value={expense?.amount || ''}
-            // why did this work. line 63
+            value={formData.amount}
             onChange={handleInputChange}
-            required
           />
         </div>
       </form>
