a39a984a506fa2ac832e9d0b455f79dde7b63906
Kurtis <kcasperson7@gmail.com>
2023-04-22 12:33:41 -0700

    Add map function to add data into new table row

--- src/components/TableRows.js
+++ src/components/TableRows.js
@@ -1,26 +1,17 @@
 import React from 'react'
-// import App from '../App'
 
 const TableRows = ({ expenses }) => {
   return (
     <>
       <tbody>
-        {/* <App expenses={expenses} /> */}
         {expenses.map((expense) => (
           <tr>
-            <td>{expense.description}</td>
+            <td>{expense.date}</td>
             <td>{expense.merchant}</td>
+            <td>{expense.paymentType}</td>
             <td>{expense.amount}</td>
-            <td></td>
           </tr>
         ))}
-
-        {/* <td>{date.addRow}</td> */}
-        {/* or add list itmes? */}
-        {/* <td>{merchant.addRow}</td> */}
-        {/* </thead> */}
-        {/* <CreateExpense /> */}
-        {/* create empty rows to be able to add data from state */}
       </tbody>
     </>
   )
