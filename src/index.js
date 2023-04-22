c80e2e619f526b46468d5d20f92fa0ecf96d4d3c
Kurtis <kcasperson7@gmail.com>
2023-04-22 12:34:37 -0700

    Remove imported components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import ExpenseTable from './components/ExpenseTable'
-import CreateExpense from './components/CreateExpense'
-import AddTableRow from './components/TableRows'
-import Header from './components/Header'
 import reportWebVitals from './reportWebVitals'
-import Form from './Practice'
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    {/* <Header />
-    <CreateExpense />
-    <ExpenseTable /> */}
     <App />
   </React.StrictMode>
 )
