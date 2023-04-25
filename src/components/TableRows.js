ac0065faa2fe23021a614fcaeac780ffe6ce8202
Kurtis <kcasperson7@gmail.com>
2023-04-24 18:10:57 -0700

    Add table rows when submit button is clicked

--- src/components/TableRows.js
+++ src/components/TableRows.js
@@ -1,18 +1,20 @@
 import React from 'react'
 
-const TableRows = ({ expenses }) => {
+const TableRows = (props) => {
+  const { removeDataRow, expenses } = props
   return (
     <>
-      <tbody>
-        {expenses.map((expense) => (
-          <tr>
-            <td>{expense.date}</td>
-            <td>{expense.merchant}</td>
-            <td>{expense.paymentType}</td>
-            <td>{expense.amount}</td>
-          </tr>
-        ))}
-      </tbody>
+      {expenses.map((expense, index) => (
+        <tr key={index}>
+          <td>{expense.date}</td>
+          <td>{expense.merchant}</td>
+          <td>{expense.paymentType}</td>
+          <td>{expense.amount}</td>
+          <td>
+            <button onClick={() => removeDataRow(expense.id)}>X</button>
+          </td>
+        </tr>
+      ))}
     </>
   )
 }
