0b49a61149a38a31cb4a633c4216445a6b1a4f36
Kurtis <kcasperson7@gmail.com>
2023-04-28 23:09:34 -0700

    Remove comments

--- src/components/CreateExpense.js
+++ src/components/CreateExpense.js
@@ -3,13 +3,9 @@ import React from 'react'
 const CreateExpense = (props) => {
   const { formData, setFormData } = props
 
-  // setFormData will refer back to the previous value when there is a change
-
   function handleInputChange(event) {
     const { name, value } = event.target
-    // dont understnad this totally
-    // my interpretation: using spread operator to copy the formdata. and then accessing
-    // name and value from the form?
+
     setFormData((prevExpense) => ({ ...prevExpense, [name]: value }))
   }
 
